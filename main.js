17607071cea3649da2e244375555bb09be40fd30
Ian Sherwood <sherwood.ian1@gmail.com>
2021-12-01 22:57:06 -0500

    make a buttone with just js

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
-import { EmojiButton } from '@joeattardi/emoji-button';
 
-const picker = new EmojiButton();
-const trigger = document.querySelector('.trigger');
-
-picker.on('emoji', selection => {
-  trigger.innerHTML = selection.emoji;
-});
-
-trigger.addEventListener('click', () => picker.togglePicker(trigger));
+// This code creates a button. 
+let btn = document.createElement("button");
+btn.innerHTML = "Click Me";
+document.body.appendChild(btn);
