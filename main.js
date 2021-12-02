2c409625732733c098aa61b6245f80222a208cb7
Ian Sherwood <sherwood.ian1@gmail.com>
2021-12-02 00:17:59 -0500

    working but now it cant identify not present

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
 
 // This code creates a button. 
+var existCondition = setInterval(function() {
+  if (window.solana) {
+     console.log("Exists!");
+     clearInterval(existCondition);
+     doTheRestOfTheStuff()
+  } else {console.log("no")}
+ }, 10); // check every 100ms
+
+
+function doTheRestOfTheStuff() {
 const isPhantomInstalled = window.solana && window.solana.isPhantom
-console.log(window.solana)
 let btn = document.createElement("button");
 console.log("wtf")
 console.log(isPhantomInstalled);
+console.log(window.solana)
 if(isPhantomInstalled == true) {
   btn.innerHTML = "Phantom Wallet Present";
 }else{
   btn.innerHTML = "Phantom Wallet Not Present";
 }
-document.body.appendChild(btn);
+document.body.appendChild(btn);}
