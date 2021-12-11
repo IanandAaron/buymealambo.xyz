0dd80eb414f7bc5670e09e39bb324f24c7dce815
Ian Sherwood <sherwood.ian1@gmail.com>
2021-12-11 12:51:29 -0500

    trying to use mui grid components

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Grid, Container } from '@mui/material';
 
 class About extends React.Component {
     render() {
         return (
-            <div className="About">
+            <div className="Home">
+                <Container maxWidth="sm">
+                    <Grid container spacing={5} rowSpacing={6} alignItems="center" columnSpacing={{ xs: 1, sm: 10, md: 50 }}>
+                        <Grid item xs={6}>
+                            <img src="Aaron.png" alt="Aaron" />
+                        </Grid>
+                        <Grid item xs={6}>
+                        <img src="Ian.jpeg" alt="Ian" />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <p1>aaron likes to make stuff bla bla bla.</p1>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <p1>Ian Stuff</p1>
+                        </Grid>
+                    </Grid>
+                </Container>
 
             </div>
         )
