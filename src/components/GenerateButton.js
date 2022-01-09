d1d61f3445450612a980403ae62d812f76bb76e7
Aaron Lee <arnnlee5@gmail.com>
2022-01-08 23:56:29 -0800

    [fix-mobile] - Fixes components for mobile

--- src/components/GenerateButton.js
+++ src/components/GenerateButton.js
@@ -65,6 +65,7 @@ function GenerateButton(props) {
           <div className="CodeBlock">
             <CopyBlock
              customStyle={{
+                overflowX: 'scroll',
                 overflowY: 'scroll',
                 borderRadius: '10px',
                 boxShadow: '1px 2px 3px rgba(0,0,0,0.35)',
